a28ff3bafe8ad2cb687f3b55530da2577ea3b861
Morrism1 <morrismontana0@gmail.com>
2020-05-28 10:31:04 +0200

    Adding react router to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import HomePage from './components/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+import Shop from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+     <Switch>
+       <Route exact path='/' component={HomePage} />
+       <Route path='/shop' component={Shop} />
+     </Switch>
     </div>
   );
 }
