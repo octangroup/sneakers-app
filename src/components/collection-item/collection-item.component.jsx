53111f89f680fca8acf81e7fcfc0c66a1220a2f4
Morrism1 <morrismontana0@gmail.com>
2020-05-29 08:26:17 +0200

    Add button to collection items that can add them to cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { connect} from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem} from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
 const priceSplitter = (number) =>'$' + (Math.round(number/ 100).toFixed(2));
 
-const CollectionItem = ({id,name,retail_price_cents,main_picture_url}) => (
+const CollectionItem = ({item,addItem}) => {
+    const { name,retail_price_cents,main_picture_url} = item;
+    return (
     <div className='collection-item'>
         <div
             className='image'
@@ -15,7 +21,12 @@ const CollectionItem = ({id,name,retail_price_cents,main_picture_url}) => (
             <span className='name'>{name}</span>
             <span className='price'>{priceSplitter(retail_price_cents)}</span>
         </div> 
+        <CustomButton onClick={()=> addItem(item)} inverted> ADD TO CART</CustomButton>
     </div>
-)
+)};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null,mapDispatchToProps) (CollectionItem);
