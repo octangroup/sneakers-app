42c4f1c7047dbc74691088028bc18334b52f40a9
Morrism1 <morrismontana0@gmail.com>
2020-05-29 07:08:22 +0200

    Add cart to header and render it to the index

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { ReactComponent as Logo} from '../../assets/shoes.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({hidden}) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo />
@@ -14,11 +17,17 @@ const Header = () => (
       <Link className="option" to="/shop">
         Shop
       </Link>
-      <Link className="option" to="/cart">
-        Cart
-      </Link>
+      <CartIcon />
     </div>
+    {
+      hidden ? null :
+      <CartDropdown />
+    }
   </div>
 );
 
-export default Header;
+const mapStateToProps = ({cart:{ hidden}}) => ({
+  hidden
+})
+
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
+,  document.getElementById("root")
 );
